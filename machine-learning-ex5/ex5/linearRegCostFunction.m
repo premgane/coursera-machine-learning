3c961ec7ca8d2386e6003cdee1fabbfcbf163d90
Prem Ganeshkumar <ganeprem@gmail.com>
2016-10-30 15:25:29 -0400

    Finished cost function and learning curve

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -20,9 +20,10 @@ grad = zeros(size(theta));
 %
 
 
+J = (1/(2*m)) * sum(( X * theta .- y ) .^ 2) + ((lambda/(2*m)) * sum(theta(2:end) .^ 2));
 
 
-
+grad = ((1/m) * ( (X*theta .- y)' * X )' ) .+ ((lambda/m) * [0; theta(2:end)]);
 
 
 
