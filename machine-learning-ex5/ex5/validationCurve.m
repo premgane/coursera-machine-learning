1f550ecafca2ecb6a04170888f9b16702aa59e19
Prem Ganeshkumar <ganeprem@gmail.com>
2016-11-24 17:52:55 -0500

    Finish ex5

--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -39,7 +39,14 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
+for i=1:length(lambda_vec),
+	lambda = lambda_vec(i);
 
+	[theta] = trainLinearReg([ones(size(X,1),1) X], y, lambda);
+
+	error_train(i) = linearRegCostFunction([ones(size(X,1),1) X], y, theta, 0);
+	error_val(i) = linearRegCostFunction([ones(size(Xval,1),1) Xval], yval, theta, 0);
+end;
 
 
 
